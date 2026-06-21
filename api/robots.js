0c7601ab57d877d768c7dbd7dd2aa4ec11b0419b
ballimsuhayl-commits <ballimsuhayl@gmail.com>
2026-06-21 10:09:21 +0200

    Cache robots file at edge

--- api/robots.js
+++ api/robots.js
@@ -2,6 +2,8 @@ const { PUBLIC_URL } = require('./catalog');
 
 module.exports = function robots(req, res) {
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.setHeader('Cache-Control', 'public, max-age=0, must-revalidate');
+  res.setHeader('Cache-Control', 'public, max-age=0, s-maxage=86400, stale-while-revalidate=604800');
+  res.setHeader('CDN-Cache-Control', 'public, max-age=86400, stale-while-revalidate=604800');
+  res.setHeader('Vercel-CDN-Cache-Control', 'public, max-age=86400, stale-while-revalidate=604800');
   res.end(`User-agent: *\nAllow: /\n\nSitemap: ${PUBLIC_URL}/sitemap.xml\n`);
 };
